3ab801ccd6ed97522915877e9160d67836a782c6
DanielRT96 <reti.daniel96@gmail.com>
2020-08-10 19:25:46 +0200

    Components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { QuizComponent } from './quiz/quiz.component';
+import { QuestionComponent } from './quiz/question/question.component';
+import { CategoriesComponent } from './categories/categories.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, QuizComponent, QuestionComponent, CategoriesComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
